bbe1aa2cd8f1c3a353fcdfe4c5dd94a256b9c562
gcaaa31928 <gcaaa31928@gmail.com>
2016-05-31 18:17:08 +0800

    added main test

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+from . import test_basic
+from . import test_app
+from . import test_main_page
+from . import test_mrt
+from . import test_public_bike
+from . import test_railway
+from . import test_taxi
